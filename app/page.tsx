6f2c9d0fcdb63d2704d8a0e478b21fb6b1852ff4
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2024-09-15 17:49:07 +0700

    trying-fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+"use client"
 import TreeSection from "@/components/section/image-section/TreeSection";
 import StartDownSection from "@/components/section/image-section/StartDownSection";
 import HorseOnCliffSection from "@/components/section/image-section/hore-on-cliff/HorseOnCliffSection";
 import ProfileSection from "@/components/section/profile/ProfileSection";
 import BgProfile from "@/components/section/profile/pice/BgProfile";
+import axios from "axios";
+import React from "react";
+import {useModal} from "@/hooks/store/use-modal-store";
 
 export default function Home() {
+    const {data}=useModal();
+    const getData = async () => {
+        return await axios.get('/api/profiles', {
+            params: {
+                t: new Date().getTime()
+            }, headers: {'Cache-Control': 'no-cache'}
+        }).then((data) => {
+            console.log("data HOME",data.data[0].title)
+        });
+    }
+    React.useEffect(()=>{
+        getData().then(r => r)
+    },[data])
     return (
         <main className="w-screen h-screen relative">
             {/** background profile **/}
