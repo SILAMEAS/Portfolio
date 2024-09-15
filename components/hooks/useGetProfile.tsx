e4a61aa6b62f8d7ab4f7f0cfc3f1b0e0b6c01658
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2024-09-15 17:24:56 +0700

    change-fetch-to-axios

--- components/hooks/useGetProfile.tsx
+++ components/hooks/useGetProfile.tsx
@@ -1,20 +1,20 @@
 import {useEffect, useState} from 'react';
 import {ProfileDto} from "@/db/dto/ProfileDto";
 import {useModal} from "@/hooks/store/use-modal-store";
+import axios from "axios";
 
 const useGetProfile = () => {
     const [profile, setProfile] = useState<ProfileDto | undefined>(undefined);
     const [isLoading, setLoading] = useState<boolean>(false);
     const { data } = useModal();
     const getData = async () => {
-        // { headers: { 'Cache-Control': 'no-cache' }}
-        await fetch('/api/profiles', {method:"GET", headers: {'Cache-Control': 'no-cache'}})
-            .then((res) => res.json())
-            .then((data) => {
-                const {id, ...res} = data[0];
-                setProfile(res)
-                setLoading(false);
-            });
+        await axios.get('/api/profiles',{ headers: { 'Cache-Control': 'no-cache' }}).then((data)=>{
+            console.log('data',data)
+            const {id, ...res} = data.data[0];
+            setProfile(res)
+            setLoading(false);
+        });
+
     }
     useEffect(() => {
         setLoading(true);
