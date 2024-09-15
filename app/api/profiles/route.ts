38cb906055d03d1ed50c8ad046d4c28e48b88f36
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2024-09-15 17:20:51 +0700

    set-header-no-cach-in-get-method

--- app/api/profiles/route.ts
+++ app/api/profiles/route.ts
@@ -2,23 +2,28 @@ import {AppDataSource, checkDBConnection} from "@/db/typeorm.config";
 import {NextResponse} from "next/server";
 import {Profile} from "@/db/entities/Profile";
 
+
 export async function GET() {
     try {
         await checkDBConnection();
         const userRepository = AppDataSource.getRepository(Profile);
-        let users: Profile[] = [];
-        users =   await userRepository.find();
-        if(users.length===0){
-            const newUser=userRepository.create({
-                mainTile:"Web Development",
-                title:"Make anything possible with",
-                description:"Hello, I`m Sila. My experience is 2 years with web development. You can \\tcontact me if you want to build your website for your business."
-                }
-            );
-            await userRepository.save(newUser);
-        }
-        return NextResponse.json(users);
+        const users: Profile[] = await userRepository.find();
+        const response = NextResponse.json(users);
+        response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        response.headers.set('Expires', '0');
+        response.headers.set('Pragma', 'no-cache');
+        return response;
     } catch (error) {
         return NextResponse.error();
     }
 }
+
+//   if(users.length===0){
+//             const newUser=userRepository.create({
+//                 mainTile:"Web Development",
+//                 title:"Make anything possible with",
+//                 description:"Hello, I`m Sila. My experience is 2 years with web development. You can \\tcontact me if you want to build your website for your business."
+//                 }
+//             );
+//             await userRepository.save(newUser);
+//         }
